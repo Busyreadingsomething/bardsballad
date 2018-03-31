f5fb0fb75c3a32ab55dd09399d6699be03cbbfdb
Makan Azarshahy <oro1919@gmail.com>
2018-03-31 14:16:38 -0700

    Added entry for bundle to render component

--- client/app.jsx
+++ client/app.jsx
@@ -1,3 +1,5 @@
-import AppView from './component/AppView';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import SheetView from './component/SheetView';
 
+ReactDOM.render(<SheetView />, document.getElementById('app'));
