a57d3faeb4417fc51d36042301c339f837096475
Makan Azarshahy <oro1919@gmail.com>
2018-04-02 11:02:26 -0700

    Corrected spelling

--- client/component/AlignView.jsx
+++ client/component/AlignView.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const AlignView = ({ setAlign }) => (
-  <div>
-    <label className="gen-align"> Pick a Class:
+  <div className="gen-align-container">
+    <label className="gen-align"> Alignment:
       <select id="gen-align" onChange={e => setAlign(e)}>
         <option value="">Select an Alignment</option>
         <option value="LAWFUL GOOD">Lawful Good</option>
         <option value="NEUTRAL GOOD">Neutral Good</option>
         <option value="CHAOTIC GOOD">Chaotic Good</option>
-        <option value="LAWFUL NUETRAL">Lawful Neutral</option>
-        <option value="NUETRAL">Neutral</option>
-        <option value="CHAOTIC NUETRAL">Chaotic Neutral</option>
+        <option value="LAWFUL NEUTRAL">Lawful Neutral</option>
+        <option value="NEUTRAL">Neutral</option>
+        <option value="CHAOTIC NEUTRAL">Chaotic Neutral</option>
         <option value="LAWFUL EVIL">Lawful Evil</option>
         <option value="NEUTRAL EVIL">Neutral Evil</option>
         <option value="CHAOTIC EVIL">Chaotic Evil</option>
