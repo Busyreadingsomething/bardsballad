7e430f5d7411e31b7e14ce87f2bbf1a2ccc10f52
Makan Azarshahy <oro1919@gmail.com>
2018-04-16 11:49:01 -0700

    Added action types file

---
+++ client/component/actionTypes/index.js
@@ -0,0 +1,9 @@
+const actionTypes = {
+  UPDATE_NAME: 'UPDATE_NAME',
+  UPDATE_CLASS: 'UPDATE_CLASS',
+  UPDATE_RACE: 'UPDATE_RACE',
+  UPDATE_ALIGN: 'UPDATE_ALIGN',
+  PICK_ROLL: 'PICK_ROLL',
+};
+
+export default actionTypes;
