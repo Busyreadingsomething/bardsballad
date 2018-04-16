80e8ab2f7d039ac5f6cbfc7ad74774cd0094a8d3
Makan Azarshahy <oro1919@gmail.com>
2018-04-16 11:50:30 -0700

    Added basic raceModifiers for testing

---
+++ practiceData/raceModifiers.js
@@ -0,0 +1,39 @@
+const raceModifiers = {
+  DRAGONBORNE: {
+    STR: 2,
+    CHA: 1,
+  },
+  DWARF: {
+    CON: 2,
+  },
+  ELF: {
+    DEX: 2,
+  },
+  GNOME: {
+    INT: 2,
+  },
+  'HALF-ELF': {
+    CHA: 2,
+  },
+  'HALF-ORC': {
+    STR: 2,
+    CON: 1,
+  },
+  HALFLING: {
+    DEX: 2,
+  },
+  HUMAN: {
+    STR: 1,
+    DEX: 1,
+    CON: 1,
+    INT: 1,
+    WIS: 1,
+    CHA: 1,
+  },
+  TIEFLING: {
+    INT: 1,
+    CHA: 2,
+  },
+};
+
+export default raceModifiers;
