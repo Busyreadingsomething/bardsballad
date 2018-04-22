ff35f0e41dcc98c09f3a9b93f66f08f7b9208c85
Makan Azarshahy <oro1919@gmail.com>
2018-04-22 13:23:31 -0700

    Added ExpProgressContainer to the character sheet view

--- client/component/CharacterView.jsx
+++ client/component/CharacterView.jsx
@@ -4,6 +4,7 @@ import WeaponsListContainer from './containers/WeaponsListContainer';
 import SpellsListContainer from './containers/SpellsListContainer';
 import AbilityListContainer from './containers/AbilityListContainer';
 import StatusContainer from './containers/StatusContainer';
+import ExpProgressContainer from './containers/ExpProgressContainer';
 
 const CharacterView = props => (
   <div className="character-container">
@@ -14,9 +15,7 @@ const CharacterView = props => (
       <SpellsListContainer />
     </div>
     <div className="bard-col-2">
-      <div className="experience-row">
-        EXPERIENCE VIEW
-      </div>
+      <ExpProgressContainer />
       <AbilityListContainer />
       <div className="character-image-container">CHARACTER IMAGE VIEW</div>
       <div className="gear-container">GEAR VIEW</div>
