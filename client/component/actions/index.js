8d01bfa7799e450deb4b779152fd5f145be25a97
Makan Azarshahy <oro1919@gmail.com>
2018-04-24 18:51:40 -0700

    Added actions for different Name, Class, Race, and Alignement update

---
+++ client/component/actions/index.js
@@ -0,0 +1,30 @@
+import actionTypes from '../actionTypes';
+
+const updateName = name => ({
+  type: actionTypes.UPDATE_NAME,
+  name: name.target.value,
+});
+
+const updateAlign = align => ({
+  type: actionTypes.UPDATE_ALIGN,
+  align,
+});
+
+const updateClass = className => ({
+  type: actionTypes.UPDATE_CLASS,
+  className,
+});
+
+const updateRace = race => ({
+  type: actionTypes.UPDATE_RACE,
+  race,
+});
+
+const actions = {
+  updateName,
+  updateAlign,
+  updateClass,
+  updateRace,
+};
+
+export default actions;
