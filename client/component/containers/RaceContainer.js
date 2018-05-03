fa60598ed1a2d16e25fcf2b6e277fcc81db571e6
Makan Azarshahy <oro1919@gmail.com>
2018-05-03 11:37:41 -0700

    Created containers with the appropriate events

---
+++ client/component/containers/RaceContainer.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import RaceView from '../RaceView';
+import actions from '../actions';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = dispatch => ({
+  setRace: e => dispatch(actions.updateRace(e)),
+});
+
+const RaceContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(RaceView);
+
+export default RaceContainer;
