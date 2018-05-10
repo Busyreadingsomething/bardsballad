f39a025f8934e4fc4a53856cd8262d6b488bdbae
Makan Azarshahy <oro1919@gmail.com>
2018-05-10 14:00:11 -0700

    Removed useless function

--- client/component/AbilityRoll.jsx
+++ client/component/AbilityRoll.jsx
@@ -2,32 +2,6 @@ import React from 'react';
 import ScoreView from './ScoreView';
 import RollButtonView from './RollButtonView';
 
-const generateRoll = () => {
-  const rolls = [];
-
-  for (let roll = 0; roll < 4; roll += 1) {
-    const num = Math.floor(Math.random() * 7);
-    if (!rolls.length) {
-      rolls.push(num);
-    } else if (rolls[0] < num) {
-      rolls.push(num);
-    } else if (rolls[0] > num) {
-      rolls.unshift(num);
-    } else {
-      rolls.push(num);
-    }
-  }
-
-  return rolls.reduce((acc, num, index) => {
-    let added = acc;
-    if (index) {
-      added += num;
-    }
-    return added;
-  }, 0);
-};
-
-
 class AbilityRoll extends React.Component {
   constructor(props) {
     super(props);
