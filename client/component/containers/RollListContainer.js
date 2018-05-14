d4696b03fa5820191772c2533e0a752f4622d610
Makan Azarshahy <oro1919@gmail.com>
2018-05-14 11:45:13 -0700

    Added setAbility to the container

--- client/component/containers/RollListContainer.js
+++ client/component/containers/RollListContainer.js
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux';
 import RollListView from '../ScoreGenViews/RollListView';
 import toJS from './toJS/index';
+import action from '../actions';
 
 const mapStateToProps = state => ({
   rolls: state.getIn(['data', 'rolls']),
 });
 
-const mapDispatchToProps = () => ({});
+const mapDispatchToProps = dispatch => ({
+  setAbility: () => dispatch(action.setAbility()),
+});
 
 const RollListContainer = connect(
   mapStateToProps,
