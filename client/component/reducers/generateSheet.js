1ff36e70306f98aca854f9324f921c4bfa4f1662
Makan Azarshahy <oro1919@gmail.com>
2018-05-18 16:30:34 -0700

    Refactored the logic to a helpers folder

--- client/component/reducers/generateSheet.js
+++ client/component/reducers/generateSheet.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 import character from '../../../practiceData/genCharacter';
 import generator from '../../../practiceData/generatorData';
-import rolls from '../../../practiceData/genRolls';
+import helpers from './helpers';
 
 const fillerScores = {
   str: 13,
@@ -51,54 +51,6 @@ const testSpells = [
   },
 ];
 
-const setAbilityStat = (state, stat) => {
-  let updatedState = state;
-  const updated = state.getIn(['data', 'holder', stat, 'selected']);
-  const val = state.getIn(['data', 'currentValue']);
-  const position = state.getIn(['data', 'currentIndex']);
-
-  if (val && !updated) {
-    updatedState = state
-      .updateIn(['data', 'currentValue'], () => null)
-      .updateIn(['data', 'currentIndex'], () => null)
-      .updateIn(['data', 'rolls', position], () => 'DONE')
-      .updateIn(['data', 'holder', stat, 'val'], () => val)
-      .updateIn(['data', 'holder', stat, 'selected'], () => true);
-  }
-
-  return updatedState;
-};
-
-const setFinalAbility = (state) => {
-  let updatedState = state;
-  const holder = state.getIn(['data', 'holder']).toJS();
-  const isReady = Object.keys(holder).every(stat => holder[stat].selected);
-
-  if (isReady) {
-    const scores = Object.keys(holder).reduce((acc, stat) => {
-      acc[stat] = holder[stat].val;
-      return acc;
-    }, {});
-    const ability = character.genAbility(scores);
-    updatedState = updatedState.updateIn(['character', 'ability'], () => fromJS(ability));
-  }
-
-  return updatedState;
-};
-
-const setRolls = (state, select) => {
-  let updatedState = state.updateIn(['data', 'rollSelect'], () => select);
-
-  if (select === 'standard') {
-    updatedState = updatedState.updateIn(['data', 'rolls'], () => fromJS(rolls.genStandard()));
-  } else if (select === 'roll') {
-    updatedState = updatedState.updateIn(['data', 'rolls'], () => fromJS(rolls.genRollList()));
-  }
-  console.log(updatedState.getIn(['data', 'rolls']));
-
-  return updatedState;
-};
-
 const blankCharacter = character.genCharacter(fillerScores, 'BORT', 'MALE', '5ft 3', 31, 'silver', 'green', 'GOOD', 'elf', 'bard', testWeapons, testSpells);
 const initialState = fromJS({
   character: blankCharacter,
@@ -108,21 +60,24 @@ const initialState = fromJS({
 const generateSheet = (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_PROFILE':
-      return state.updateIn(action.prop, () => action.value);
+      return state
+        .updateIn(action.prop, () => action.value);
     case 'UPDATE_RACE':
-      return state.updateIn(['character', 'race'], () => fromJS(character.genRace(action.race)));
+      return state
+        .updateIn(['character', 'race'], () => fromJS(character.genRace(action.race)));
     case 'UPDATE_CLASS':
-      return state.updateIn(['character', 'charClass'], () => fromJS(character.genClass(action.className)));
+      return state
+        .updateIn(['character', 'charClass'], () => fromJS(character.genClass(action.className)));
     case 'PICK_ROLL':
-      return setRolls(state, action.rollSelect);
+      return helpers.setRolls(state, action.rollSelect);
     case 'SET_SCORE':
       return state
         .updateIn(['data', 'currentValue'], () => action.val)
         .updateIn(['data', 'currentIndex'], () => action.index);
     case 'UPDATE_HOLDER':
-      return setAbilityStat(state, action.stat);
+      return helpers.setAbilityStat(state, action.stat);
     case 'SET_ABILITY':
-      return setFinalAbility(state);
+      return helpers.setFinalAbility(state);
     default:
       return state;
   }
