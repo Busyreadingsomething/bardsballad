28d04e7348b723a2c279e21eb8b113429b87f0c2
Makan Azarshahy <oro1919@gmail.com>
2018-06-16 10:10:18 -0700

    Removed AbilityRoll and its container

--- client/component/GenerateView.jsx
+++ client/component/GenerateView.jsx
@@ -2,43 +2,20 @@ import React from 'react';
 import ProfileGenView from './ProfileGenView';
 import ScoreGenContainer from './containers/ScoreGenContainer';
 import ClassContainer from './containers/ClassContainer';
-import AbilityRollContainer from './containers/AbilityRollContainer';
 import AlignContainer from './containers/AlignContainer';
 import AttributeSelectorContainer from './containers/AttributeSelectorContainer';
 
-class GenerateView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      point: null,
-    };
-  }
-
-  render() {
-    const { point } = this.state;
-    return (
-      <div className="gen-container">
-        <ProfileGenView />
-        <ScoreGenContainer />
-        <div className="gen-dropdowns">
-          <ClassContainer />
-          <AlignContainer />
-        </div>
-        {
-          point === 'roll' ? <AbilityRollContainer
-            standard={[]}
-          /> : null
-        }
-        {
-          point === 'standard' ? <AbilityRollContainer
-            standard={[15, 14, 13, 12, 10, 18]}
-          /> : null
-        }
-        <AttributeSelectorContainer />
-        <button className="gen-submit" onClick={this.props.generate}>SUBMIT</button>
-      </div>
-    );
-  }
-}
+const GenerateView = props => (
+  <div className="gen-container">
+    <ProfileGenView />
+    <ScoreGenContainer />
+    <div className="gen-dropdowns">
+      <ClassContainer />
+      <AlignContainer />
+    </div>
+    <AttributeSelectorContainer />
+    <button className="gen-submit" onClick={props.generate}>SUBMIT</button>
+  </div>
+);
 
 export default GenerateView;
