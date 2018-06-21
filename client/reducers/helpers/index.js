35f50209fcc05a7783b5ef1fa738410a882b73df
Makan Azarshahy <oro1919@gmail.com>
2018-06-21 11:02:46 -0700

    Moved reducer helper functions to this file, and cleaned up methods for updating Immutable objects

--- client/reducers/helpers/index.js
+++ client/reducers/helpers/index.js
@@ -5,13 +5,13 @@ import rolls from '../../../practiceData/genRolls';
 const setAbilityStat = (state, stat) => {
   let updatedState = state;
   const updated = state.getIn(['holder', stat, 'selected']);
-  const val = state.getIn(['currentValue']);
-  const position = state.getIn(['currentIndex']);
+  const val = state.get('currentValue');
+  const position = state.get('currentIndex');
 
   if (val && !updated) {
     updatedState = state
-      .updateIn(['currentValue'], () => null)
-      .updateIn(['currentIndex'], () => null)
+      .update('currentValue', () => null)
+      .update('currentIndex', () => null)
       .updateIn(['rolls', position], () => 'DONE')
       .updateIn(['holder', stat, 'val'], () => val)
       .updateIn(['holder', stat, 'selected'], () => true);
@@ -22,8 +22,8 @@ const setAbilityStat = (state, stat) => {
 
 const setFinalAbility = (state) => {
   let updatedState = state;
-  const holder = state.getIn(['holder']).toJS();
-  const proficiencies = state.getIn(['proficiencies']).toJS();
+  const holder = state.get('holder').toJS();
+  const proficiencies = state.get('proficiencies').toJS();
   const isReady = Object.keys(holder).every(stat => holder[stat].selected);
 
   if (isReady) {
@@ -38,27 +38,58 @@ const setFinalAbility = (state) => {
     });
     const ability = character.genAbility(scores);
     updatedState = updatedState
-      .updateIn(['ability'], () => fromJS(ability))
-      .updateIn(['proficiencies'], () => fromJS(proficiencies));
+      .update('ability', () => fromJS(ability))
+      .update('proficiencies', () => fromJS(proficiencies));
   }
 
   return updatedState;
 };
 
 const setRolls = (state, select) => {
-  let updatedState = state.updateIn(['rollSelect'], () => select);
+  let updatedState = state.update('rollSelect', () => select);
 
   if (select === 'standard') {
-    updatedState = updatedState.updateIn(['rolls'], () => fromJS(rolls.genStandard()));
+    updatedState = updatedState.update('rolls', () => fromJS(rolls.genStandard()));
   } else if (select === 'roll') {
-    updatedState = updatedState.updateIn(['rolls'], () => fromJS(rolls.genRollList()));
+    updatedState = updatedState.update('rolls', () => fromJS(rolls.genRollList()));
   }
 
   return updatedState;
+};
+
+const setProfile = (state, action) => state.updateIn(action.prop, () => action.value);
+
+const setRace = (state, action) => state.update('race', () => fromJS(character.genRace(action.race)));
+
+const setClass = (state, action) => state.update('charClass', () => fromJS(character.genClass(action.className)));
+
+const setAttribute = (state, action) => {
+  const { attr, prof, index } = action;
+  let newState = state;
+  const proficient = state.getIn([prof, attr, 'proficient']);
+  const rule = state.getIn(['charClass', prof, index, 'rule']);
+
+  if (proficient) {
+    newState = state
+      .updateIn([prof, attr, 'val'], val => val - 2)
+      .updateIn([prof, attr, 'proficient'], selected => !selected)
+      .updateIn(['charClass', prof, index, 'rule'], () => rule + 1);
+  } else if (rule !== 0) {
+    newState = state
+      .updateIn([prof, attr, 'val'], val => val + 2)
+      .updateIn([prof, attr, 'proficient'], selected => !selected)
+      .updateIn(['charClass', prof, index, 'rule'], () => rule - 1);
+  }
+
+  return newState;
 };
 
 export default {
   setAbilityStat,
   setFinalAbility,
   setRolls,
+  setProfile,
+  setRace,
+  setClass,
+  setAttribute,
 };
