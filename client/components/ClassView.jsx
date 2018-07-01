aa8dafc083b2da87b77238d3bd41fdf802cbb29d
Makan Azarshahy <oro1919@gmail.com>
2018-07-01 14:41:04 -0700

    Added more styles and adjusted the classes for the components

--- client/components/ClassView.jsx
+++ client/components/ClassView.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import { func } from 'prop-types';
 
 const ClassView = ({ setClass }) => (
-  <div className="gen-class-container">
-    <label className="gen-class"> Class:
-      <select id="character-charClass" onChange={e => setClass(e)}>
-        <option value="">Select a Class</option>
-        <option value="barbarian">Barbarian</option>
-        <option value="bard">Bard</option>
-        <option value="cleric">Cleric</option>
-        <option value="druid">Druid</option>
-        <option value="fighter">Fighter</option>
-        <option value="monk">Monk</option>
-        <option value="paladin">Paladin</option>
-        <option value="ranger">Ranger</option>
-        <option value="rogue">Rogue</option>
-        <option value="sorceror">Sorceror</option>
-        <option value="warlock">Warlock</option>
-        <option value="wizard">Wizard</option>
-      </select>
-    </label>
-  </div>
+  <label className="gen-label"> Class
+    <select className="gen-profile-input" onChange={e => setClass(e)}>
+      <option value="">Select a Class</option>
+      <option value="barbarian">Barbarian</option>
+      <option value="bard">Bard</option>
+      <option value="cleric">Cleric</option>
+      <option value="druid">Druid</option>
+      <option value="fighter">Fighter</option>
+      <option value="monk">Monk</option>
+      <option value="paladin">Paladin</option>
+      <option value="ranger">Ranger</option>
+      <option value="rogue">Rogue</option>
+      <option value="sorceror">Sorceror</option>
+      <option value="warlock">Warlock</option>
+      <option value="wizard">Wizard</option>
+    </select>
+  </label>
 );
 
 ClassView.propTypes = {
