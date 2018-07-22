ee0c5bdc2016c337d6c77248a9d22ea08596e070
Makan Azarshahy <oro1919@gmail.com>
2018-07-21 22:58:24 -0700

    Added generateIndex

--- client/components/PlayerImageView.jsx
+++ client/components/PlayerImageView.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { arrayOf, string, number } from 'prop-types';
 
+const generateIndex = (max, curr) => {
+  const ratio = Math.floor((max / curr) * 100);
+  switch (ratio) {
+    case ratio >= 80:
+      return 4;
+    case ratio >= 60:
+      return 3;
+    case ratio >= 40:
+      return 2;
+    case ratio >= 20:
+      return 1;
+    default:
+      return 0;
+  }
+};
+
 const PlayerImageView = ({ images, maxHP, currHP }) => {
+  const index = generateIndex(maxHP, currHP);
   return (
     <div>
-      <img src="" alt="character" />
+      <img src={images[index]} alt="character" />
     </div>
   );
 };
