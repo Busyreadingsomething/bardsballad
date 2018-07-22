431375c185bfd792e698afdeb66a338fd0eef76b
Makan Azarshahy <oro1919@gmail.com>
2018-07-21 22:57:51 -0700

    Added final props

--- client/components/PlayerImageView.jsx
+++ client/components/PlayerImageView.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { arrayOf, string } from 'prop-types';
+import { arrayOf, string, number } from 'prop-types';
 
-const PlayerImageView = ({ images }) => {
-  
+const PlayerImageView = ({ images, maxHP, currHP }) => {
   return (
     <div>
-      <img src="" />
+      <img src="" alt="character" />
     </div>
-  )
+  );
 };
 
 PlayerImageView.propTypes = {
   images: arrayOf(string).isRequired,
+  maxHP: number.isRequired,
+  currHP: number.isRequired,
 };
 
 export default PlayerImageView;
