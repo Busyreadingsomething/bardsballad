33713b5cb79df28a322719a73da4416182743245
Makan Azarshahy <oro1919@gmail.com>
2018-07-22 17:08:45 -0700

    Added stage-2 to webpack babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['env', 'react'],
+          presets: ['env', 'react', 'stage-2'],
         },
       },
     ],
