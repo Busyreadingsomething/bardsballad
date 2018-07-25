ae86a2cbac4b9a638f9001af1f50df0750477375
Makan Azarshahy <oro1919@gmail.com>
2018-07-24 22:43:41 -0700

    Added general GearView component

---
+++ client/components/GearViewList/GearView.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ItemView from './ItemView';
+
+const GearView = ({ items }) => (
+  <div className="gear-container">
+    {
+      items.map(item => <ItemView key={item.id} item={item} />)
+    }
+  </div>
+);
+
+GearView.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default GearView;
