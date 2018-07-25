9ae10b5d537140ad66798f5fe346e960f1f39124
Makan Azarshahy <oro1919@gmail.com>
2018-07-24 23:02:55 -0700

    Added practice data to render the objects

--- client/components/GearListView.jsx
+++ client/components/GearListView.jsx
@@ -1,45 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import GearView from './GearViewList/GearView';
+import genGear from '../../practiceData/item';
 
-const items = [
-  {
-    id: 'w1234',
-    dieType: 6,
-    name: 'Dagger',
-    dice: 1,
-    description: 'This is test dagger.',
-    proficient: true,
-    bonus: 3,
-  },
-  {
-    id: 'w1112',
-    dieType: 8,
-    name: 'Long Sword',
-    dice: 1,
-    description: 'This is a test long sword.',
-    proficient: true,
-    bonus: 1,
-  },
-  {
-    id: 'w0034',
-    dieType: 4,
-    name: 'AXE',
-    dice: 1,
-    description: 'This is test AXE.',
-    proficient: true,
-    bonus: 3,
-  },
-  {
-    id: 'w9902',
-    dieType: 8,
-    name: 'SCYTHE',
-    dice: 1,
-    description: 'Okay this is supposed to be a super secret long descriptive item name that is just a placeholder for more information.',
-    proficient: true,
-    bonus: 1,
-  },
-]
+const gear = genGear();
 
 class GearListView extends React.Component {
   constructor(props) {
@@ -56,6 +20,8 @@ class GearListView extends React.Component {
 
   render() {
     const { current, tabs } = this.state;
+    // be sure to get the gear from the redux store
+    const items = gear[current.toLowerCase()];
     return (
       <div className="gear-container card">
         <p className="card-title">Equipment</p>
