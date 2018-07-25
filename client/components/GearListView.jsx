5c19b59cc451bcca42e0d8ae04b994c64dd01949
Makan Azarshahy <oro1919@gmail.com>
2018-07-24 22:29:17 -0700

    Added GearView and changed tab to current for the local state

--- client/components/GearListView.jsx
+++ client/components/GearListView.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import GearView from './GearViewList/GearView';
 
 class GearListView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tab: 'Weapons',
+      current: 'Weapons',
       tabs: ['Weapons', 'Armor', 'Spells', 'Gear'],
     };
   }
 
-  handleClick = () => {
-    console.log('HELLO THERE');
+  handleClick = (tab) => {
+    console.log('HELLO THERE', tab);
   }
 
   render() {
-    const { tabs } = this.state;
+    const { current, tabs } = this.state;
     return (
       <div className="gear-container card">
         <p className="card-title">Equipment</p>
         <div className="tab-selection">
           {
-            tabs.map(tab => <p key={tab} tab={tab} className="tab" onClick={this.handleClick}>{tab}</p>)
+            tabs.map(tab => <p key={tab} tab={tab} className="tab" onClick={() => this.handleClick(tab)}>{tab}</p>)
           }
         </div>
+        <GearView tab={current} items="OBJECT_FROM_REDUX"/>
       </div>
     );
   }
