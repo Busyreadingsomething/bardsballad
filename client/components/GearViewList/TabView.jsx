a11d56908a94a14afe5c9274fa8455571c97cac4
Makan Azarshahy <oro1919@gmail.com>
2018-07-26 12:06:07 -0700

    hanged Fragment to div with primary color

--- client/components/GearViewList/TabView.jsx
+++ client/components/GearViewList/TabView.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { arrayOf, string, func } from 'prop-types';
 
 const Tab = ({ tabs, select }) => (
-  <React.Fragment>
+  <div className="tab-selection primary">
     {
       tabs.map(tab => (
         <button
           key={tab}
           tab={tab}
-          className="tab"
+          className="tab primary"
           onClick={() => select(tab)}
         >{tab}
         </button>))
     }
-  </React.Fragment>
+  </div>
 );
 
 Tab.propTypes = {
